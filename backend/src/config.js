601c08b9b4e39890d2386240ce9e901914c0cd5a
Angelo Knappett <angeloknappett2@gmail.com>
2022-03-20 07:05:35 -0400

    Update Info

--- backend/src/config.js
+++ backend/src/config.js
@@ -71,7 +71,7 @@ const TOKENS_PER_MINT = 1; // maximum number of NFTs a user can mint in a single
 const PUBLIC_MINT_START_DATE = "2022-03-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = "2022-03-20T11:05:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 250; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0xcC69b90032f3Ca3a36D9FA2Cf213Af9a3deEe1A7"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri
