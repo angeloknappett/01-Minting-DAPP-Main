6573a952d47706784e313f3141212a428207c7d5
Angelo Knappett <angeloknappett2@gmail.com>
2022-04-21 00:00:09 -0400

    gg

--- backend/src/config.js
+++ backend/src/config.js
@@ -64,8 +64,8 @@ const METADATA_UPDATABLE = true; // set to false if you don't want to allow meta
 const OWNER_ADDRESS = '0xcC69b90032f3Ca3a36D9FA2Cf213Af9a3deEe1A7';
 const TREASURY_ADDRESS = '0xcC69b90032f3Ca3a36D9FA2Cf213Af9a3deEe1A7';
 const MAX_SUPPLY = 400; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
-const MINT_PRICE = 8; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
-const TOKENS_PER_MINT = 5; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
+const MINT_PRICE = 1; // Minting price per NFT. Rinkeby = ETH, Polygon = MATIC. CANNOT BE UPDATED!
+const TOKENS_PER_MINT = 1; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
 const PUBLIC_MINT_START_DATE = "2022-04-20T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
